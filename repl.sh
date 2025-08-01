92b7a207d6a6b38e02e58e16740f4ac8e5e3cf65
Jarkko Saltiola <jarkko@saltiolacode.fi>
2025-08-01 17:58:02 +0300

    REPL command for VVV (Vagrant)

--- repl.sh
+++ repl.sh
@@ -1,3 +1,9 @@
-# TODO
-~/.local/bin/bvv ssh
-./vendor/bin/phel repl
+# Helper script defining project REPL command (experimental)
+
+# Using:
+# - VVV https://github.com/Varying-Vagrant-Vagrants/VVV/
+# - bvv tool https://codeberg.org/jasalt/bvv
+bvv ssh vendor/bin/phel repl
+
+# To use with docker compose see docs https://github.com/jasalt/phel-wp-plugin e.g. (not tested):
+# docker compose exec -w /var/www/html/wp-content/plugins/woocommerce-memberships-migrator wordpress vendor/bin/phel repl
