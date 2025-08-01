f9392f07eb21638ae591d2c80bb1e7f2bd9c1f52
Jarkko Saltiola <jarkko@saltiolacode.fi>
2025-08-01 18:55:30 +0300

    absolute path to bvv

--- repl.sh
+++ repl.sh
@@ -3,7 +3,7 @@
 # Using:
 # - VVV https://github.com/Varying-Vagrant-Vagrants/VVV/
 # - bvv tool https://codeberg.org/jasalt/bvv
-bvv ssh vendor/bin/phel repl
+~/.local/bin/bvv ssh vendor/bin/phel repl
 
 # To use with docker compose see docs https://github.com/jasalt/phel-wp-plugin e.g. (not tested):
 # docker compose exec -w /var/www/html/wp-content/plugins/woocommerce-memberships-migrator wordpress vendor/bin/phel repl
